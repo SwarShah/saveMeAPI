7c2e8d2c38e897b4f0c4a1c4fe5e0b73265e60cc
SwarShah <swar12894@gmail.com>
2015-03-31 16:32:13 -0400

    Removed code for mail fucntion

--- api.php
+++ api.php
@@ -18,11 +18,7 @@ function sendMsg($params){
 	return $output;
 }
 function sendEmail($email, $location){
-	$to = $email;
-	$subject = "Emergency";
-	$message = $location;
-	$from = "no-reply@mySaveMeApp.com";
-	mail($to, $subject, $message, $from);
+
 }
 if(isset($_POST['phone'], $_POST['location'], $_POST['email'])){
 	//echo "Got everything";
