2149a1ac81acb3e462484672e65c1600945bd9a4
SwarShah <swar12894@gmail.com>
2015-03-31 16:41:59 -0400

    Using PHPMailer to send email

--- api.php
+++ api.php
@@ -1,7 +1,6 @@
 <?php
 require_once('class.phpmailer.php');
 include_once("class.smtp.php");
-require_once("../credentials.php");
 function sendMsg($params){
 	$url="URL for API";
 	$postData = "";
@@ -21,7 +20,25 @@ function sendMsg($params){
 	return $output;
 }
 function sendEmail($email, $location){
-
+	include_once("../credentials.php");
+	$mail = new PHPMailer();
+	$mail->IsSMTP();
+	$mail->SMTPDebug  = 2;
+	$mail->SMTPAuth   = true;
+	$mail->Host       = $mailHost;
+	$mail->Port       = $mailPort;
+	$mail->Username   = $mailUsername;
+	$mail->Password   = $mailPassword;
+	$mail->SetFrom('no-reply@swar.webs.pm', 'saveME');
+	$mail->Subject    = "Emergency";
+	$mail->MsgHTML($location);
+	$address = $email;
+	$mail->AddAddress($address);
+	if(!$mail->Send()) {
+	  echo "Mailer Error: " . $mail->ErrorInfo;
+	} else {
+	  echo "Mail sent!";
+	}
 }
 if(isset($_POST['phone'], $_POST['location'], $_POST['email'])){
 	//echo "Got everything";
